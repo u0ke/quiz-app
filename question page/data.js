dfd29a31923119ff5ae0e20e16a523147c3dace3
Hamza Bari <barihamza73@gmail.com>
2026-04-13 16:59:48 +0100

    css

---
+++ question page/data.js
@@ -0,0 +1,66 @@
+const quizData = {
+    "HTML": {
+        icon: "../menu page/assets/Vector2.png",
+        iconBg: "#FFF5ED",
+        questions: [
+            {
+                question: "What does HTML stand for?",
+                options: ["Hyper Text Markup Language", "Home Tool Markup Language", "Hyperlinks and Text Markup Language", "Hyper Tool Multi Language"],
+                answer: "Hyper Text Markup Language"
+            },
+            {
+                question: "Choose the correct HTML element for the largest heading:",
+                options: ["<heading>", "<h6>", "<head>", "<h1>"],
+                answer: "<h1>"
+            }
+        ]
+    },
+    "CSS": {
+        icon: "../menu page/assets/Vector3.png",
+        iconBg: "#E0FDEF",
+        questions: [
+            {
+                question: "What does CSS stand for?",
+                options: ["Computer Style Sheets", "Cascading Style Sheets", "Colorful Style Sheets", "Creative Style Sheets"],
+                answer: "Cascading Style Sheets"
+            },
+            {
+                question: "Which HTML attribute is used to define inline styles?",
+                options: ["class", "style", "font", "styles"],
+                answer: "style"
+            }
+        ]
+    },
+    "JavaScript": {
+        icon: "../menu page/assets/Vector4.png",
+        iconBg: "#EBF0FF",
+        questions: [
+            {
+                question: "Inside which HTML element do we put the JavaScript?",
+                options: ["<scripting>", "<js>", "<script>", "<javascript>"],
+                answer: "<script>"
+            },
+            {
+                question: "How do you write 'Hello World' in an alert box?",
+                options: ["msgBox('Hello World');", "alertBox('Hello World');", "msg('Hello World');", "alert('Hello World');"],
+                answer: "alert('Hello World');"
+            }
+        ]
+    },
+    "Accessibility": {
+        icon: "../menu page/assets/Vector5.png",
+        iconBg: "#F6E7FF",
+        questions: [
+            {
+                question: "Which of these color contrast ratios defines the minimum WCAG 2.1 Level AA requirement for normal text?",
+                options: ["4.5 : 1", "3 : 1", "2.5 : 1", "5 : 1"],
+                answer: "4.5 : 1"
+            },
+            {
+                question: "Which attribute is used to provide alternative text for an image?",
+                options: ["alt", "title", "src", "description"],
+                answer: "alt"
+            }
+        ]
+    }
+};
